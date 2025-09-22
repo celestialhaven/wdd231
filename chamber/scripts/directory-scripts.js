916364768e487736bacf87ec4933cc2c6c8c5b9c
celestialhaven <jonzmabz04@gmail.com>
2025-09-23 00:18:15 +0800

    week 02 update footer section

--- chamber/scripts/directory-scripts.js
+++ chamber/scripts/directory-scripts.js
@@ -51,3 +51,19 @@ try {
     themeBtn?.setAttribute('aria-pressed', 'true');
   }
 } catch {}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // current year
+  const currentYear = new Date().getFullYear();
+  const yearElement = document.getElementById("currentyear");
+  if (yearElement) {
+    yearElement.textContent = ` ${currentYear}`;
+  }
+
+  // last modified date of the document
+  const lastModified = document.lastModified;
+  const modifiedElement = document.getElementById("lastModified");
+  if (modifiedElement) {
+    modifiedElement.textContent = `Last Modified: ${lastModified}`;
+  }
+});
